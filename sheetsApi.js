e9e530013cc0aaa59a20ca13a5c2007c718594f4
Sam Park <SAMAP@uci.edu>
2023-02-05 23:06:48 -0800

    First prototype with inventory displayed + text and tag search functionality implemented

--- sheetsApi.js
+++ sheetsApi.js
@@ -8,15 +8,232 @@ const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4'
 // included, separated by spaces.
 const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
 
-const SPREADSHEET_ID = '1bkOsHBlgEeVSQQWGd9n0E9ywy_K419WTP0Cqv5hd-QI'
-const RANGE = 'A2:I50';
+const SPREADSHEET_ID = '1bkOsHBlgEeVSQQWGd9n0E9ywy_K419WTP0Cqv5hd-QI';
+const RANGE = 'A2:I1000000';
 
+var LIST_OF_ITEMS = [];
+var CHECKED_TAGS_SET = new Set();
+var TAGS_SET = new Set();
+var COUNTER = -1;
 
 /**
  * Callback after api.js is loaded.
  */
 function gapiLoaded() {
     gapi.load('client', initializeGapiClient);
+}
+
+/**
+ * Modifies the list of tags that are checked
+ */
+function modifyCheckedTagList(modifiedTag) {
+    if (modifiedTag.target.checked) {
+        CHECKED_TAGS_SET.add(modifiedTag.target.name);
+    } else {
+        CHECKED_TAGS_SET.delete(modifiedTag.target.name);
+    }
+    search();
+}
+
+/**
+ * Checks to see if an item has all the tags currently checked
+ */
+function checkTagsOfItem(currentItemTags) {
+    if (currentItemTags.length < CHECKED_TAGS_SET.size) {
+        return false;
+    }
+
+    for (const checkedTag of CHECKED_TAGS_SET.values()) {
+        tagExists = false;
+        for (let i = 0; i < currentItemTags.length; i++) {
+            if (currentItemTags[i].textContent === checkedTag) {
+                tagExists = true;
+                break;
+            }
+        }
+
+        if (!tagExists) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+/**
+ * Adds a new card to html
+ */
+function loadData(list_of_items) {
+    console.log("LOADING DATA");
+    COUNTER = 0;
+    for (let i = 0; i < list_of_items.length; i++) {
+        /*
+        0:      Name                "Name"                          Note: Self-explanatory
+        1:      Photo               "Google Drive URL"              Note: Should find another way instead of using a link that may change
+        2:      Quantity            "0" or more                     Note: Should avoid negative count
+        3:      Location            "1 - Cubby"                     Note: Check Sheets for alternatives
+        4:      Tags                "Game, Multiplayer, Wii"        Note: Delimited by ','
+        5:      Subtitle            "PC/Desktop"                    Note: Empty fields possible
+        6:      Description         "Missing the Disc"              Note: Empty fields possible
+        7:      Condition           "1 - Poor"                      Note: At the moment, only 1, 2, 3 possible
+        8:      Officer Notes       "Find disc"                     Note: Notes
+         */
+
+        // TODO: Implement helper functions for clean and modular code
+        if (typeof(list_of_items[i][0]) !== "undefined" && list_of_items[i][0].length > 0) {
+            COUNTER++;
+
+            // To place items underneath <div id="database">
+            var database = document.getElementById("database");
+
+            // Make <article class ="item">
+            var article = document.createElement("article");
+            article.className = "item";
+
+            // Make <header class="item-header">
+            var header = document.createElement("header");
+            header.className = "item-header";
+
+            // Make <h1> item name </h1>
+            var name = document.createElement("h1");
+            var nameText = document.createTextNode(list_of_items[i][0]);
+            name.appendChild(nameText);
+
+            // Make <div class="item-wrap">
+            var itemWrap = document.createElement("div");
+            itemWrap.className = "item-wrap";
+
+            // Make <section class="item-description">
+            var itemDescription = document.createElement("section");
+            itemDescription.className = "item-description";
+
+            // Load item description
+            try {
+                var itemDescriptionTextParagraph = document.createElement("p");
+                var itemDescriptionText = document.createTextNode("");
+                if (typeof list_of_items[i][6] !== "undefined") {
+                    itemDescriptionText = document.createTextNode(list_of_items[i][6]);
+                }
+                itemDescriptionTextParagraph.appendChild(itemDescriptionText);
+                itemDescription.appendChild(itemDescriptionTextParagraph);
+            }
+            catch(err) {
+                console.log("Missing item description");
+            }
+
+            // Make <section class="item-image">
+            var itemImage = document.createElement("section");
+            itemImage.className = "item-image";
+
+            // Load item-image full-res
+            var image = document.createElement("a");
+            image.href = list_of_items[i][1]; // TODO: URL for image
+            image.target = "_blank";
+
+            // Load item-image thumbnail
+            var imageThumbnail = document.createElement("img");
+            imageThumbnail.src = list_of_items[i][1]; // TODO: URL for thumbnail
+            imageThumbnail.alt = list_of_items[i][0];
+            image.appendChild(imageThumbnail);
+            
+            // Make <div class="item-metadata">
+            var itemMetadata = document.createElement("div");
+            itemMetadata.className = "item-metadata";
+
+            // Make <section class="item-tags">
+            var itemTag = document.createElement("section");
+            itemTag.className = "item-tags";
+
+            // Load item-tags
+            var tagsUnorderedList = document.createElement("ul");
+
+            try {
+                var conditionStatus = list_of_items[i][7].charAt(0);
+                var conditionTag = document.createElement("li");
+                if (conditionStatus == "1") {
+                    var currentConditionText = document.createTextNode("Poor Condition");
+                    conditionTag.appendChild(currentConditionText);
+                } else if (conditionStatus == "2") {
+                    var currentConditionText = document.createTextNode("Good Condition");
+                    conditionTag.appendChild(currentConditionText);
+                } else if (conditionStatus == "3") {
+                    var currentConditionText = document.createTextNode("Unopened");
+                    conditionTag.appendChild(currentConditionText);
+                }
+                tagsUnorderedList.appendChild(conditionTag);
+            }
+            catch(err) {
+                console.log("Missing item condition");
+            }
+
+            // TODO: enums and other constants implementations
+            try {
+                TAGS_SET.add("Poor Condition");
+                TAGS_SET.add("Good Condition");
+                TAGS_SET.add("Unopened");
+                var listOfTags = new Set(list_of_items[i][4].split(", "));
+                for (specificTag of listOfTags.values()) {
+                    TAGS_SET.add(specificTag);
+                    var currentTag = document.createElement("li");
+                    var currentTagText = document.createTextNode(specificTag);
+                    currentTag.appendChild(currentTagText);
+                    tagsUnorderedList.appendChild(currentTag);
+                }
+                itemTag.appendChild(tagsUnorderedList);
+            }
+            catch(err) {
+                console.log("Missing item tags");
+            }
+
+            // Make <section class="item-count">
+            try {
+                var itemCount = document.createElement("section");
+                itemCount.className = "item-count";
+                var itemCountParagraph = document.createElement("p");
+                var itemCountNumber = document.createTextNode(" Count Unknown");
+                if (typeof list_of_items[i][2] !== "undefined") {
+                    itemCountNumber = document.createTextNode(list_of_items[i][2]);
+                }
+                itemCountParagraph.appendChild(itemCountNumber);
+                itemCount.appendChild(itemCountParagraph);
+            }
+            catch(err) {
+                console.log("Missing item count");
+            }
+
+            itemMetadata.appendChild(itemTag);
+            itemMetadata.appendChild(itemCount);
+            itemImage.appendChild(image);
+            itemWrap.appendChild(itemDescription);
+            itemWrap.appendChild(itemImage);
+            itemWrap.appendChild(itemMetadata);
+            header.appendChild(name);
+            article.appendChild(header);
+            article.appendChild(itemWrap);
+            database.appendChild(article);
+        }
+    }
+    // Load Tags To Search Bar
+    // To place items underneath <div id="database">
+    var searchTagsLocation = document.getElementsByClassName("tag-filter-list")[0];
+    for (specificTag of TAGS_SET.values()) {
+        var currentTag = document.createElement("div");
+        currentTag.className = "tag";
+        var tagType = document.createElement("input");
+        tagType.type = "checkbox";
+        tagType.onchange = modifyCheckedTagList.bind(this); 
+        tagType.name = specificTag;
+        tagType.id = "tag-" + specificTag;
+        currentTag.appendChild(tagType);
+
+        var tagLabel = document.createElement("label");
+        tagLabel.htmlFor = "tag-" + specificTag;
+        tagLabel.appendChild(document.createTextNode(specificTag));
+        currentTag.appendChild(tagLabel);
+        searchTagsLocation.appendChild(currentTag);
+    }
+    document.getElementsByClassName("total-count-number")[0].textContent = COUNTER;
+    console.log("FINISHED LOADING DATA");
 }
 
 /**
@@ -31,8 +248,10 @@ async function initializeGapiClient() {
     });
     console.log("initialized");
     console.log("fetching data");
-    await getData();
-    console.log("fetched");
+    LIST_OF_ITEMS = await getData();
+    console.log(LIST_OF_ITEMS);
+    console.log("fetched data");
+    await loadData(LIST_OF_ITEMS);
 }
 
 
@@ -58,7 +277,15 @@ async function getData() {
         console.log("no values found");
         return;
     }
+    // Sort the arrays ascending order; Ex) 0 to 9, then A to Z
+    range['values'].sort();
 
-    console.log(range);
-    return;
+    // Remove empty arrays or arrays with empty string for item name
+    let index = 0;
+    while (range['values'][index].length === 0 || range['values'][index][0] === '') {
+        index++;
+        // Implement a break if neither while condition is true
+    }
+
+    return range['values'].slice(index);
 }
